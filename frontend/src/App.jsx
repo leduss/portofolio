ebd14165dd0287b474e7ad7ad33fc784d56d28bd
Julien DUSSART <julien.dussart59@gmail.com>
2022-11-27 14:25:28 +0100

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div className="">
+      <p className="bg-red-600">coucou</p>
     </div>
   );
 }
