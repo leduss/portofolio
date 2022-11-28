0dd964df7eded1a9fbe8aab5d9f1be9deb2bc140
Julien DUSSART <julien.dussart59@gmail.com>
2022-11-28 13:55:13 +0100

    add tools

---
+++ frontend/src/components/Onglets/tools/data.js
@@ -0,0 +1,89 @@
+import html from "./assets/html-1.svg";
+import css from "./assets/css-3.svg";
+import js from "./assets/javascript-1.svg";
+import react from "./assets/react-2.svg";
+import tailwind from "./assets/tailwind-css-2.svg";
+import express from "./assets/express-109.svg";
+import node from "./assets/nodejs-icon.svg";
+import mysql from "./assets/mysql-6.svg";
+import vscode from "./assets/visual-studio-code-1.svg";
+import github from "./assets/github-icon.svg";
+import git from "./assets/git-icon.svg";
+import figma from "./assets/figma-1.svg";
+
+const data = [
+  {
+    id: 1,
+    svg: html,
+    name_language: "HTML",
+    id_style: 2,
+  },
+  {
+    id: 2,
+    svg: css,
+    name_language: "CSS",
+    id_style: 2,
+  },
+  {
+    id: 3,
+    svg: js,
+    name_language: "JAVASCRIPT",
+    id_style: 2,
+  },
+  {
+    id: 4,
+    svg: react,
+    name_language: "REACT",
+    id_style: 2,
+  },
+  {
+    id: 5,
+    svg: tailwind,
+    name_language: "TAILWIND",
+    id_style: 2,
+  },
+  {
+    id: 6,
+    svg: express,
+    name_language: "EXPRESS",
+    id_style: 3,
+  },
+  {
+    id: 7,
+    svg: node,
+    name_language: "NODE JS",
+    id_style: 3,
+  },
+  {
+    id: 8,
+    svg: mysql,
+    name_language: "MY SQL",
+    id_style: 3,
+  },
+  {
+    id: 9,
+    svg: vscode,
+    name_language: "VISUAL STUDIO CODE",
+    id_style: 4,
+  },
+  {
+    id: 10,
+    svg: github,
+    name_language: "GITHUB",
+    id_style: 4,
+  },
+  {
+    id: 11,
+    svg: git,
+    name_language: "GIT",
+    id_style: 4,
+  },
+  {
+    id: 1,
+    svg: figma,
+    name_language: "FIGMA",
+    id_style: 4,
+  },
+];
+
+export default data;
---
+++ frontend/src/components/Onglets/tools/style.js
@@ -0,0 +1,16 @@
+const style = [
+  {
+    id_style: 2,
+    name_style: "FRONT END",
+  },
+  {
+    id_style: 3,
+    name_style: "BACK END",
+  },
+  {
+    id_style: 2,
+    name_style: "TOOLS",
+  },
+];
+
+export default style;
